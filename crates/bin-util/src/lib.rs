1712ddfd5af81af1e5746d9eadd73cd4f675292e
Angelos Stylianidis <agathangelos.stylianidis@gmail.com>
2025-09-23 14:56:13 +0300

    feat(bin-util): handle signals using tokio::signal

--- crates/bin-util/src/lib.rs
+++ crates/bin-util/src/lib.rs
@@ -17,6 +17,7 @@ use opentelemetry::metrics::Counter;
 use opentelemetry::{KeyValue, global};
 use serde::{Deserialize as _, Deserializer};
 use tokio_util::sync::CancellationToken;
+use tracing::{error, trace};
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_error::ErrorLayer;
 use tracing_opentelemetry::OpenTelemetryLayer;
@@ -110,28 +111,32 @@ pub fn init_logging(
 }
 
 /// Register cancel token and ctrl+c handler
-///
-/// # Panics
-///   on failure to register ctr+c handler
 #[allow(
     clippy::print_stdout,
     reason = "not a tracing msg, should always display"
 )]
-#[allow(dead_code, reason = "temporary")]
 #[must_use]
 pub fn register_cancel() -> CancellationToken {
     let cancel_token = CancellationToken::new();
-    let ctrlc_token = cancel_token.clone();
-    ctrlc::set_handler(move || {
-        if ctrlc_token.is_cancelled() {
-            #[expect(clippy::restriction, reason = "immediate exit")]
-            std::process::exit(1);
-        } else {
-            println!("\nGraceful shutdown initiated. Press Ctrl+C again for immediate exit...");
-            ctrlc_token.cancel();
+
+    tokio::task::spawn({
+        let cancel_token = cancel_token.clone();
+        async move {
+            tokio::select! {
+                res = tokio::signal::ctrl_c() => {
+                    if let Err(e) = res {
+                        error!("Failed to register ctrl+c handler: {e}");
+                    }
+                    trace!("Graceful shutdown initiated.");
+                    cancel_token.cancel();
+                }
+                // This branch is to ensure that if the token is cancelled from elsewhere,
+                // we don't leave the ctrl+c handler dangling.
+                () = cancel_token.cancelled() => {}
+            }
         }
-    })
-    .expect("Failed to register ctrl+c handler");
+    });
+
     cancel_token
 }
 
